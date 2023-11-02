be9e96b38f7372ef7277594553669f30284581dd
Win Cheng <winzamark12@gmail.com>
2023-11-01 23:03:59 -0700

    AboutUsPage; Added SVGs and finished Section 1

--- frontend/src/pages/AboutUsPage.js
+++ frontend/src/pages/AboutUsPage.js
@@ -2,10 +2,18 @@ import { useAuth0 } from "@auth0/auth0-react";
 import './css/aboutUsPage.scss';
 import NavBar from "../components/NavBar";
 import tempUser from '../api/user';
-import hollowCloud from '../assets/aboutUs_assets/clouds copy_page-0016-modified 1.svg'
-import cloud from '../assets/aboutUs_assets/clouds copy_page-0016-modified 1.svg'
 import logo from '../logo.svg'
 
+function importAllSVGs(r) {
+    let images = {};
+    r.keys().map((item, index) => {
+        images[item.replace('./', '')] = r(item);
+        return true;
+    });
+    return images;
+}
+
+const svgImages = importAllSVGs(require.context('../assets/aboutUs_assets', false, /\.svg$/));
 const AboutUsPage = () => {
     const { user, isAuthenticated } = useAuth0();
 
@@ -13,13 +21,40 @@ const AboutUsPage = () => {
         (
             <div className="aboutUs">
                 {/* <NavBar /> */}
-                Temp About US PAGE
 
                 <div className="aboutUs_section1">
                     <div className="aboutUs_section1_hollowCloud"
-                        style={{ backgroundImage: `url(${cloud})` }}
+                        style={{ backgroundImage: `url(${svgImages['hollowCloud.svg']})` }}
+                    ></div>
+                    <div className="aboutUs_section1_topLeftCloud"
+                        style={{ backgroundImage: `url(${svgImages['topLeftCloud.svg']})` }}
+                    ></div>
+                    <div className="aboutUs_section1_smallCloud"
+                        style={{ backgroundImage: `url(${svgImages['smallCloud.svg']})` }}
                     ></div>
 
+                    <div className="aboutUs_section1_topCenterCloud"
+                        style={{ backgroundImage: `url(${svgImages['topCenterCloud.svg']})` }}
+                    ></div>
+
+                    <div className="aboutUs_section1_topRightCloud"
+                        style={{ backgroundImage: `url(${svgImages['topRightCloud.svg']})` }}
+                    ></div>
+
+                    <div className="aboutUs_section1_bottomRightCloud"
+                        style={{ backgroundImage: `url(${svgImages['bottomRightCloud.svg']})` }}
+                    ></div>
+                    <div className="aboutUs_section1_cowCloud"
+                        style={{ backgroundImage: `url(${svgImages['cowCloud.svg']})` }}
+                    ></div>
+
+                    <div className="aboutUs_section1_Cow"
+                        style={{ backgroundImage: `url(${svgImages['Cow.svg']})` }}
+                    ></div>
+
+                    <div className="aboutUs_section1_caption">
+                        <span style={{ fontFamily: "Gluten", color: "whitesmoke" }}>"Every Shot is a memory."</span>
+                    </div>
                 </div>
                 <div className="aboutUs_section2"></div>
                 <div className="aboutUs_section3"></div>
