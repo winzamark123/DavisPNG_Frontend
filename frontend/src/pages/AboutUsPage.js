1a4221c9216be287fe81d46db0f5631818844d45
Win Cheng <winzamark12@gmail.com>
2023-11-01 17:13:17 -0700

    AboutUsPage; Creating Structure

--- frontend/src/pages/AboutUsPage.js
+++ frontend/src/pages/AboutUsPage.js
@@ -1,7 +1,10 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import './aboutUsPage.scss';
+import './css/aboutUsPage.scss';
 import NavBar from "../components/NavBar";
 import tempUser from '../api/user';
+import hollowCloud from '../assets/aboutUs_assets/clouds copy_page-0016-modified 1.svg'
+import cloud from '../assets/aboutUs_assets/clouds copy_page-0016-modified 1.svg'
+import logo from '../logo.svg'
 
 const AboutUsPage = () => {
     const { user, isAuthenticated } = useAuth0();
@@ -9,8 +12,17 @@ const AboutUsPage = () => {
     return (
         (
             <div className="aboutUs">
-                <NavBar />
-               Temp About US PAGE
+                {/* <NavBar /> */}
+                Temp About US PAGE
+
+                <div className="aboutUs_section1">
+                    <div className="aboutUs_section1_hollowCloud"
+                        style={{ backgroundImage: `url(${cloud})` }}
+                    ></div>
+
+                </div>
+                <div className="aboutUs_section2"></div>
+                <div className="aboutUs_section3"></div>
             </div>
         )
     )
