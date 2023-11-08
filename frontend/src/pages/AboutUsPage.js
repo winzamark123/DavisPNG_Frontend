65073c06ecca0d8fba46c2c4dd7cbc7d56c09bbc
Win Cheng <winzamark12@gmail.com>
2023-11-08 13:11:57 -0800

    AboutUsPage; Adjusting to larger res

--- frontend/src/pages/AboutUsPage.js
+++ frontend/src/pages/AboutUsPage.js
@@ -24,98 +24,80 @@ const AboutUsPage = () => {
         (
             <div className="aboutUs">
                 <NavBar />
+                <div className="aboutUs_section1_background">
+                    <div className="aboutUs_section1">
+                        <div className="aboutUs_section1_coverBG"></div>
+                        <div className="aboutUs_section1_title"
+                            style={{ backgroundImage: `url(${svgImages['DavisPNGTitle.svg']})` }}
+                        ></div>
+                        <div className="aboutUs_section1_caption">
+                            <span style={{ fontFamily: "Mada" }}>Every Shot is a memory.</span>
+                        </div>
+                        <div className="aboutUs_section1_collage">
 
-                <div className="aboutUs_section1">
-                    <div className="aboutUs_section1_coverBG"></div>
-                    <div className="aboutUs_section1_title"
-                        style={{ backgroundImage: `url(${svgImages['DavisPNGTitle.svg']})` }}
-                    ></div>
-                    <div className="aboutUs_section1_caption">
-                        <span style={{ fontFamily: "Mada" }}>Every Shot is a memory.</span>
-                    </div>
-                    <div className="aboutUs_section1_collage">
-
+                        </div>
                     </div>
                 </div>
+
                 <div className="aboutUs_topRightCloud"
                     style={{ backgroundImage: `url(${svgImages['topRightCloud.svg']})` }}
                 ></div>
 
-                <div className="aboutUs_section2">
-                    <div className="aboutUs_section2_hollowCloud"
-                        style={{ backgroundImage: `url(${svgImages['hollowCloud.svg']})` }}
-                    ></div>
-                    <div className="aboutUs_section2_topLeftCloud"
-                        style={{ backgroundImage: `url(${svgImages['topLeftCloud.svg']})` }}
-                    ></div>
-                    <div className="aboutUs_section2_smallCloud"
-                        style={{ backgroundImage: `url(${svgImages['smallCloud.svg']})` }}
-                    ></div>
-
-                    <div className="aboutUs_section2_topCenterCloud"
-                        style={{ backgroundImage: `url(${svgImages['topCenterCloud.svg']})` }}
-                    ></div>
-
-                    <div className="aboutUs_section2_bottomRightCloud"
-                        style={{ backgroundImage: `url(${svgImages['bottomRightCloud.svg']})` }}
-                    ></div>
-                    {/* <div className="aboutUs_section2_backgroundCloud"
-                        style={{ backgroundImage: `url(${svgImages['backgroundCloud.svg']})` }}
-                    ></div> */}
-                    {/* <div className="aboutUs_section2_cowCloud"
-                        style={{ backgroundImage: `url(${svgImages['cowCloud.svg']})` }}
-                    ></div> */}
-
-                    <div className="aboutUs_section2_Cow2"
-                        style={{ backgroundImage: `url(${svgImages['Cow2.svg']})` }}
-                    ></div>
-                    <div className="aboutUs_section2_text">
-                        <div className="aboutUs_section2_text_title">
-                            <span style={{ fontFamily: "Mada", fontWeigh: 700 }}>About Us</span>
-                        </div>
-                        <div className="aboutUs_section2_text_info">
-                            <span style={{ fontFamily: "Mada", fontWeight: 500 }}>We are a group of students from UC Davis who are passionate about photography. We started this project to help people find the perfect photographer for their special events. We hope that our website can help you find the photographer that you are looking for!</span>
-                        </div>
-
-                    </div>
-                </div>
-                <div className="aboutUs_section3">
-                    <div className="aboutUs_section3_title">
-                        <span style={{ fontFamily: "Mada", fontWeight: 700 }}>CAPTURE YOUR MOMENT</span>
-                    </div>
-                    <div className="aboutUs_section3_info">
-                        <div className="aboutUs_section3_info_left">
-                            <div className="aboutUs_section3_info_left_img"
-                                style={{ backgroundImage: `url(${svgImages['cowAndCam.svg']})` }}>
+                <div className="aboutUs_section2_background">
+                    <div className="aboutUs_section2">
+                        <div className="aboutUs_section2_Cow2"
+                            style={{ backgroundImage: `url(${svgImages['Cow2.svg']})` }}
+                        ></div>
+                        <div className="aboutUs_section2_text">
+                            <div className="aboutUs_section2_text_title">
+                                <span style={{ fontFamily: "Mada", fontWeight: 700 }}>About Us</span>
                             </div>
-                            <span>Find your photographer.</span>
-                        </div>
-                        <div className="aboutUs_section3_info_mid">
-                            <div className="aboutUs_section3_info_mid_img"
-                                style={{ backgroundImage: `url(${svgImages['cowChat.svg']})` }}>
+                            <div className="aboutUs_section2_text_info">
+                                <span style={{ fontFamily: "Mada", fontWeight: 500 }}>We are a group of students from UC Davis who are passionate about photography. We started this project to help people find the perfect photographer for their special events. We hope that our website can help you find the photographer that you are looking for!</span>
                             </div>
-                            <span>Create an account to chat with our photographers</span>
-                        </div>
-                        <div className="aboutUs_section3_info_right">
-                            <div className="aboutUs_section3_info_right_img"
-                                style={{ backgroundImage: `url(${svgImages['book.svg']})` }}
-                            ></div>
-                            <span>Make your bookings.</span>
-                        </div>
 
-                    </div>
-                    <div className="aboutUs_section3_button">
-                        <span>Get Started</span>
+                        </div>
                     </div>
                 </div>
 
+                <div className="aboutUs_section3_background">
+                    <div className="aboutUs_section3">
+                        <div className="aboutUs_section3_title">
+                            <span style={{ fontFamily: "Mada", fontWeight: 700 }}>CAPTURE YOUR MOMENT</span>
+                        </div>
+                        <div className="aboutUs_section3_info">
+                            <div className="aboutUs_section3_info_left">
+                                <div className="aboutUs_section3_info_left_img"
+                                    style={{ backgroundImage: `url(${svgImages['cowAndCam.svg']})` }}>
+                                </div>
+                                <span>Find your photographer.</span>
+                            </div>
+                            <div className="aboutUs_section3_info_mid">
+                                <div className="aboutUs_section3_info_mid_img"
+                                    style={{ backgroundImage: `url(${svgImages['cowChat.svg']})` }}>
+                                </div>
+                                <span>Create an account to chat with our photographers</span>
+                            </div>
+                            <div className="aboutUs_section3_info_right">
+                                <div className="aboutUs_section3_info_right_img"
+                                    style={{ backgroundImage: `url(${svgImages['book.svg']})` }}
+                                ></div>
+                                <span>Make your bookings.</span>
+                            </div>
+
+                        </div>
+                        <div className="aboutUs_section3_button">
+                            <span>Get Started</span>
+                        </div>
+                    </div>
+                </div>
                 <div className="aboutUs_section4">
 
                 </div>
                 {/* <LandingPage /> */}
                 <Footer />
             </div >
-            
+
         )
     )
 }
