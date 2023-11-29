134d14718789653fbc69b8760fa68f088c88be91
Win Cheng <winzamark12@gmail.com>
2023-11-29 10:47:12 -0800

    Portfolio Page Linked with UUID from Backend

--- frontend/src/pages/PortfolioPage.js
+++ frontend/src/pages/PortfolioPage.js
@@ -1,19 +1,43 @@
 import { useParams } from 'react-router-dom';
 import './css/profilePage.scss';
 import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
 
-import { getPhotographers } from '../api/userAPI';
+import { fetchPhotographers } from '../api/marketplaceAPI';
 
-const getPhotographer = (uuid) => {
-    const photographers = getPhotographers();
-    return photographers.find((photographer) => photographer.uuid === uuid);
+const getPhotographer = async (uuid) => {
+    try {
+        const response = await fetchPhotographers();
+        const photographers = response.body;
+        return photographers.find((photographer) => photographer.uuid === uuid);
+    } catch (error) {
+        console.error("Error fetching photographers:", error);
+    }
+
 }
 
 const PortfolioPage = () => {
     const { uuid } = useParams();
+    const [photographer, setPhotographer] = useState(null);
 
-    const photographer = getPhotographer(uuid);
+    useEffect(() => {
+        const loadPhotographer = async () => {
+            try {
+                const photographer = await getPhotographer(uuid);
+                if (photographer) {
+                    setPhotographer(photographer);
+                }
+            } catch (error) {
+                console.error("Error fetching photographers:", error);
+            }
+        };
 
+        loadPhotographer();
+    }, [uuid]);
+
+
+    console.log(photographer)
+    if (!photographer) return (<div>Loading...</div>)
 
     return (
         (
@@ -22,8 +46,8 @@ const PortfolioPage = () => {
                 <div className="profile_container">
                     <div className="profile">
                         <div className="profile_left">
-                            <div className="profile_left_pic" style={{ backgroundImage: `url(${photographer.profilePic})` }}>
-                            </div>
+                            {/* <div className="profile_left_pic" style={{ backgroundImage: `url(${photographer.profilePic})` }}></div> */}
+
                             <div className="profile_left_text">
                                 <div className="profile_left_text_row">Name: {photographer.fname}</div>
                                 <div className="profile_left_text_row">Price: {photographer.price}</div>
